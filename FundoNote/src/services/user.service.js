7bbd26722afc02952afcdb0cc0bb38bdb98efd8c
RahulAid <raccess97@gmail.com>
2023-01-30 15:29:21 +0530

    [Rahul]Add:Used Hashing for Password Encryption

--- FundoNote/src/services/user.service.js
+++ FundoNote/src/services/user.service.js
@@ -1,4 +1,5 @@
 import User from '../models/user.model';
+const bcrypt = require('bcrypt')
 
 //get all users
 export const getAllUsers = async () => {
@@ -8,15 +9,16 @@ export const getAllUsers = async () => {
 
 //create new user
 export const userRegistration = async (body) => {
-  const userexist = await User.findOne({ email: body.email });
-  if (!userexist) {
+  const existingUser = await User.findOne({ email: body.email });
+  if (!existingUser) {
+    const salt = await bcrypt.genSalt(10);
+    body.password = await bcrypt.hash(body.password, salt);
     const data = await User.create(body);
     return data;
   }
   else {
-    throw new Error("Email ID already exists")
+    throw new Error("User already exist")
   }
-
 };
 
 
@@ -26,16 +28,16 @@ export const login = async (body) => {
   try {
     const userdata = await User.findOne({ email: body.email });
     if (!userdata) {
-      throw new Error("Invalid Credentials")
+      throw new Error("Invalid Email ID entered")
     }
-    else {
-      return 'Login Successful';
+    const validPassword = await bcrypt.compare(body.password,userdata.password);
+    if(!validPassword){
+      throw new Error("Invalid Password")
     }
-
+    return userdata;
   }
   catch (error) {
     throw new Error(error)
   }
 
 };
-
